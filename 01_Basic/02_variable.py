f8648b2da409c4268f4acbf7040698c4cce889e6
zenithavro <zenithavro7@gmail.com>
2025-12-15 23:53:02 +0600

    variable basic

---
+++ 01_Basic/02_variable.py
@@ -0,0 +1,14 @@
+# variable in python 
+
+month = "December"
+
+print(id(month)) # id returns the memory address of the variable
+
+
+counter = 100          # Creates an integer variable
+miles   = 1000.0       # Creates a floating point variable
+name    = "Zara Ali"   # Creates a string variable
+
+print (counter)
+print (miles)
+print (name)
